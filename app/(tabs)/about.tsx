0f4a8b5f933de7bf5d5eca37f186f4e059e93d5d
olesia.oleksiuk <olesia.oleksiuk@progforce.com>
2025-01-20 15:26:23 +0300

    add expo-router bottom tab navigator

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,11 +1,9 @@
 import { Text, View, StyleSheet } from "react-native";
 
-export default function Index() {
+export default function AboutScreen() {
   return (
-    <View
-      style={styles.container}
-    >
-      <Text style={styles.text}>Home Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>About screen</Text>
     </View>
   );
 }
@@ -13,9 +11,9 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#25292e",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#25292e",
   },
   text: {
     color: "#fff",
