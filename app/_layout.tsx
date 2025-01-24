274159f408b6a0bfc39816097b3eb8459b49fe76
olesia.oleksiuk <olesia.oleksiuk@progforce.com>
2025-01-24 12:17:10 +0300

    setup nativewind (tailwind)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,12 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import '../global.css'
+import { verifyInstallation } from "nativewind";
 
 export default function RootLayout() {
+
+  verifyInstallation();
+
   return (
     <>
     <StatusBar style="light" />
